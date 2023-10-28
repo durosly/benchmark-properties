6dc671d28f3142e4cf4a623b31de7f8a4fbfb5fc
durosly <slyboydon1@gmail.com>
2023-10-28 01:46:39 +0100

    ✅initial commit

---
+++ app/(public)/layout.jsx
@@ -0,0 +1,152 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/logos/logo-main.png";
+import logoWithText from "@/logos/logo-main-with-text.png";
+import NavLink from "./components/nav-links";
+
+export default function PublicLayout({ children }) {
+	return (
+		<div className="drawer">
+			<input
+				id="my-drawer-3"
+				type="checkbox"
+				className="drawer-toggle"
+			/>
+			<div className="drawer-content block">
+				{/* Navbar */}
+				<div className="max-w-7xl mx-auto">
+					<div className="w-full navbar mt-3 ">
+						<div className="navbar-start">
+							<div className="flex-none lg:hidden">
+								<label
+									htmlFor="my-drawer-3"
+									aria-label="open sidebar"
+									className="btn btn-square btn-ghost"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										className="inline-block w-6 h-6 stroke-current"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
+											d="M4 6h16M4 12h16M4 18h16"
+										></path>
+									</svg>
+								</label>
+							</div>
+							<div className="flex-none px-2 mx-2 relative w-32 h-16">
+								<Image
+									src={logo}
+									alt="benchmark realestate"
+									className="object-contain"
+									fill
+									priority={true}
+									placeholder="blur"
+								/>
+							</div>
+						</div>
+						<div className="navbar-center">
+							<div className="flex-none hidden lg:block">
+								<ul className="menu menu-horizontal">
+									{/* Navbar menu content here */}
+									<li>
+										<NavLink path="/">Home</NavLink>
+									</li>
+									<li>
+										<NavLink path="/about-us">
+											About Us
+										</NavLink>
+									</li>
+									<li>
+										<NavLink path="/rooms">Rooms</NavLink>
+									</li>
+									<li>
+										<NavLink path="/locations">
+											Locations
+										</NavLink>
+									</li>
+									<li>
+										<NavLink path="/services">
+											Services
+										</NavLink>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div className="navbar-end">
+							<a className="btn btn-primary">Find Apartment</a>
+						</div>
+					</div>
+					{/* Page content here */}
+					<main className="px-5 sm:px-10 ">{children}</main>
+				</div>
+				<footer className="footer p-10 bg-black/10 ">
+					<aside>
+						<div className="relative h-[180px] aspect-square">
+							<Image
+								src={logoWithText}
+								fill
+								placeholder="blur"
+								className="object-contain"
+							/>
+						</div>
+						<p>
+							Benchmark Ltd.
+							<br />
+							Realty Management since 2008
+						</p>
+					</aside>
+					<nav>
+						<header className="footer-title">Services</header>
+						<a className="link link-hover">Design</a>
+						<a className="link link-hover">Marketing</a>
+						<a className="link link-hover">Advertisement</a>
+					</nav>
+					<nav>
+						<header className="footer-title">Company</header>
+						<a className="link link-hover">About us</a>
+						<a className="link link-hover">Contact</a>
+						<a className="link link-hover">Locations</a>
+						<a className="link link-hover">Rooms</a>
+					</nav>
+					<nav>
+						<header className="footer-title">Legal</header>
+						<a className="link link-hover">
+							Terms &amp; conditions
+						</a>
+						<a className="link link-hover">Policies</a>
+						{/* <a className="link link-hover">Cookie policy</a> */}
+					</nav>
+				</footer>
+				<footer className="footer footer-center p-4 bg-black/20">
+					<aside>
+						<p>
+							Copyright © 2023 - All right reserved by Benchmark
+							studentpad Ltd
+						</p>
+					</aside>
+				</footer>
+			</div>
+			<div className="drawer-side">
+				<label
+					htmlFor="my-drawer-3"
+					aria-label="close sidebar"
+					className="drawer-overlay"
+				></label>
+				<ul className="menu p-4 w-80 min-h-full bg-base-200">
+					{/* Sidebar content here */}
+					<li>
+						<a>Sidebar Item 1</a>
+					</li>
+					<li>
+						<a>Sidebar Item 2</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
---
+++ app/(public)/page.jsx
@@ -0,0 +1,378 @@
+import { FiMapPin, FiSearch, FiMap, FiWifi, FiUnlock } from "react-icons/fi";
+import { BsHouse } from "react-icons/bs";
+import frontApartment from "@/public/images/front-apartment.png";
+import studyArea from "@/public/images/study-area.png";
+import livingArea from "@/public/images/living-area.png";
+import kitchen from "@/public/images/kitchen.jpg";
+import Image from "next/image";
+import Link from "next/link";
+
+function Home() {
+	return (
+		<>
+			<div className="min-h-[450px] pt-28 px-5 text-center md:text-left md:px-20 pb-10 md:pb-0 housing bg-cover bg-center rounded-2xl mt-10 mb-20">
+				<h2 className="text-6xl text-white font-bold font-roboto max-w-xl">
+					It&apos;s time to find a smart home
+				</h2>
+				<p className="text-white text-xl mt-10 mb-10 md:mb-10">
+					We are a new kind of student living experience, designed
+					with you in mind.
+				</p>
+
+				<button className="btn btn-primary md:hidden">
+					Find a home
+				</button>
+
+				<div className="mt-10 relative max-w-3xl hidden md:block">
+					<div className="bg-white max-w-[50%] py-5 px-10 rounded-t-xl relative">
+						<p className="text-xl font-bold text-primary">
+							Find exactly what you need
+						</p>
+						<div
+							style={{
+								"--size": "15px",
+								"--c1": "transparent",
+								"--c2": "#fff",
+							}}
+							className="corner absolute bottom-0 -right-[var(--size)] -rotate-90"
+						></div>
+					</div>
+					<form
+						action=""
+						className="bg-white rounded-tr-xl py-5 px-10 flex flex-wrap items-end gap-5"
+					>
+						<div className="form-control flex-1">
+							<label
+								className="label justify-start gap-2"
+								htmlFor="location"
+							>
+								<FiMapPin className="w-5 h-5" />
+								<span>Location</span>
+							</label>
+							<select
+								name="location"
+								id="location"
+								className="select select-bordered"
+							>
+								<option value="">-- select location --</option>
+								<option value="warri">Warri</option>
+								<option value="abuja">Abuja</option>
+								<option value="port">Port Harcourt</option>
+							</select>
+						</div>
+						<div className="form-control flex-1">
+							<label
+								className="label justify-start gap-2"
+								htmlFor="property"
+							>
+								<BsHouse className="w-5 h-5" />
+								<span>Property Type</span>
+							</label>
+							<select
+								name="property"
+								id="property"
+								className="select select-bordered"
+							>
+								<option value="">-- select property --</option>
+								<option value="warri">Warri</option>
+								<option value="abuja">Abuja</option>
+								<option value="port">Port Harcourt</option>
+							</select>
+						</div>
+						<div className="form-control flex-1">
+							<label
+								className="label justify-start gap-2"
+								htmlFor="price"
+							>
+								<span className="text-xl ">&#8358;</span>
+								<span>Price Range</span>
+							</label>
+							<select
+								name="price"
+								id="price"
+								className="select select-bordered"
+							>
+								<option value="">-- select price --</option>
+								<option value="warri">Warri</option>
+								<option value="abuja">Abuja</option>
+								<option value="port">Port Harcourt</option>
+							</select>
+						</div>
+						<div>
+							<button className="btn btn-primary">
+								<FiSearch />
+								Search
+							</button>
+						</div>
+					</form>
+					<div
+						style={{
+							"--size": "15px",
+							"--c1": "transparent",
+							"--c2": "#fff",
+						}}
+						className="corner absolute bottom-0 -left-[var(--size)] -rotate-180"
+					></div>
+					<div
+						style={{
+							"--size": "15px",
+							"--c1": "transparent",
+							"--c2": "#fff",
+						}}
+						className="corner absolute bottom-0 -right-[var(--size)] -rotate-90"
+					></div>
+				</div>
+			</div>
+
+			<div className="flex justify-center flex-wrap gap-5 mb-20">
+				<div className="flex-[300px] flex-grow-0">
+					<FiMap className="w-8 h-8 mx-auto stroke-primary" />
+					<div className="text-center">
+						<h3 className="text-xl font-bold my-2	">
+							Transportation
+						</h3>
+						<p className="text-sm">
+							<span className="font-bold">
+								Quick, safe and reliable.
+							</span>{" "}
+							<span>
+								We provide transportation for students moving
+								from thier apartments to school
+							</span>
+						</p>
+					</div>
+				</div>
+				<div className="flex-[300px] flex-grow-0">
+					<FiWifi className="w-8 h-8 mx-auto stroke-primary" />
+					<div className="text-center">
+						<h3 className="text-xl font-bold my-2	">
+							Unlimited Internet
+						</h3>
+						<p className="text-sm">
+							<span className="font-bold">
+								High speed over 10mb/s.
+							</span>{" "}
+							<span>
+								Internet is an essential tool to being a student
+								and we are no stranger to that
+							</span>
+						</p>
+					</div>
+				</div>
+				<div className="flex-[300px] flex-grow-0">
+					<FiUnlock className="w-8 h-8 mx-auto stroke-primary" />
+					<div className="text-center">
+						<h3 className="text-xl font-bold my-2	">Security</h3>
+						<p className="text-sm">
+							<span className="font-bold">
+								24/7 CCTV and security.
+							</span>{" "}
+							<span>
+								Safety of lives and properties is our priority.
+								We ensure to keep a tight belt on this
+							</span>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="md:flex gap-10 mb-20">
+				<div className="relative w-full md:w-96 h-52 md:h-auto aspect-square rounded-2xl overflow-hidden">
+					<Image
+						src={frontApartment}
+						alt=""
+						fill
+						className="object-cover"
+					/>
+				</div>
+				<div className="flex-1 space-y-4 mt-5 md:mt-0">
+					<h2 className="text-3xl font-semibold text-left">
+						Benchmark Studentpad
+					</h2>
+					<p>
+						We&apos;ve set out to redefine the student living
+						experience. We believe that every student deserves a
+						comfortable, safe, and inspiring place to call home
+						while pursuing their educational dreams.
+					</p>
+					<p>What Sets Us Apart:</p>
+					<ul className="pl-5 space-y-3">
+						<li>
+							<div className="flex gap-2 items-center">
+								<div className="inline-block w-2 h-2 rounded-full bg-primary"></div>
+								<h4 className="font-bold">
+									Tailored for Students
+								</h4>
+							</div>
+							<p className="pl-4">
+								We&apos;ve designed our accommodations
+								specifically for the needs of students. From
+								well-appointed private rooms to spacious common
+								areas, we provide a variety of options to suit
+								your preferences and budget.
+							</p>
+						</li>
+						<li>
+							<div className="flex gap-2 items-center">
+								<div className="inline-block w-2 h-2 rounded-full bg-primary"></div>
+								<h4 className="font-bold">
+									Safety and Security
+								</h4>
+							</div>
+							<p className="pl-4">
+								Your peace of mind is our top priority. Our
+								properties are equipped with state-of-the-art
+								security measures, including secure entry
+								systems and 24/7 surveillance, ensuring that you
+								feel safe at all times.
+							</p>
+						</li>
+						<li>
+							<div className="flex gap-2 items-center">
+								<div className="inline-block w-2 h-2 rounded-full bg-primary"></div>
+								<h4 className="font-bold">
+									Convenient Locations
+								</h4>
+							</div>
+							<p className="pl-4">
+								Our properties are strategically located near
+								major universities and colleges, making it easy
+								to get to your classes, libraries, and
+								extracurricular activities. We understand the
+								value of a convenient location in your academic
+								journey.
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className="flex gap-5 justify-center flex-wrap mb-20 ">
+				<div className="flex-1 text-center">
+					<p className="text-2xl md:text-4xl text-primary font-bold">
+						600+
+					</p>
+					<p>Apartments</p>
+				</div>
+				<div className="flex-1 text-center">
+					<p className="text-2xl md:text-4xl text-primary font-bold">
+						24+
+					</p>
+					<p>Properties</p>
+				</div>
+				<div className="flex-1 text-center">
+					<p className="text-2xl md:text-4xl text-primary font-bold">
+						60+
+					</p>
+					<p>Tenants</p>
+				</div>
+				<div className="flex-1 text-center">
+					<p className="text-2xl md:text-4xl text-primary font-bold">
+						80+
+					</p>
+					<p>Locations</p>
+				</div>
+			</div>
+
+			<div className="md:flex md:gap-5 md:h-80 mb-20 space-y-5 md:space-y-0 ">
+				<div className="flex-1 flex flex-col justify-center md:p-5">
+					<h2 className="text-xl font-bold mb-5">
+						Explore some of the best facilities
+					</h2>
+					<p>
+						We invite you to explore our exceptional properties and
+						see for yourself how we're redefining the way students
+						live during their university years.
+					</p>
+				</div>
+				<div className="flex-1 h-80 md:h-auto bg-primary relative rounded-2xl overflow-hidden">
+					<Image
+						src={studyArea}
+						alt=""
+						fill
+						className="object-cover"
+					/>
+					<div className="overlay bg-gradient-to-t from-black absolute top-0 left-0 h-full w-full flex items-end">
+						<div className="px-5 py-2 text-white">
+							<h3 className="font-bold text-xl mb-3">
+								Study area
+							</h3>
+							<p>
+								Enjoy the comfort of the cozy chairs and the
+								support of helpful assistants.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="flex-1 h-80 md:h-auto bg-primary relative rounded-2xl overflow-hidden">
+					<Image
+						src={livingArea}
+						alt=""
+						fill
+						className="object-cover"
+					/>
+					<div className="overlay bg-gradient-to-t from-black absolute top-0 left-0 h-full w-full flex items-end">
+						<div className="px-5 py-2 text-white">
+							<h3 className="font-bold text-xl mb-3">
+								Living area
+							</h3>
+							<p>
+								This vibrant space is designed for relaxation
+								and unwinding after a long day at school.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="flex-1 h-80 md:h-auto bg-primary relative rounded-2xl overflow-hidden">
+					<Image
+						src={kitchen}
+						alt=""
+						fill
+						className="object-cover"
+					/>
+					<div className="overlay bg-gradient-to-t from-black absolute top-0 left-0 h-full w-full flex items-end">
+						<div className="px-5 py-2 text-white">
+							<h3 className="font-bold text-xl mb-3">Kitchen</h3>
+							<p>
+								Step into our well-equipped kitchen, where
+								culinary creativity knows no bounds.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="bg-primary p-10 rounded-xl text-center mb-20 relative">
+				<p className="text-4xl font-bold mb-5">
+					Find Your Dream Room Using Our Interactive Catalog
+				</p>
+				<Link
+					href="/nice"
+					className="btn btn-wide"
+				>
+					Check all houses
+				</Link>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="59.1162"
+					height="45.35772"
+					viewBox="0 0 59.1162 45.35772"
+					creator="Katerina Limpitsouni"
+					className="absolute bottom-10 left-5 md:left-24"
+				>
+					<path
+						d="M41.69158,14.65737c3.02152-4.51246,6.04304-9.02491,9.06455-13.53737h-1.29521c2.40533,3.96877,4.81066,7.93755,7.216,11.90632,.34353,.56682,.68706,1.13365,1.03059,1.70047,.49968,.82446,1.79734,.07144,1.29521-.75708-2.40533-3.96877-4.81066-7.93755-7.216-11.90632-.34353-.56682-.68706-1.13365-1.03059-1.70047-.3042-.50192-.98348-.46555-1.29521,0-3.02152,4.51246-6.04304,9.02491-9.06455,13.53737-.53858,.80434,.76019,1.55609,1.29521,.75708h0Z"
+						fill="#01010C"
+						origin="undraw"
+					/>
+					<path
+						d="M.71568,45.06023c9.37198-1.04012,18.03249-6.72685,22.51593-15.06658,2.28371-4.24797,3.7883-9.90346,1.05639-14.31396-2.29329-3.70239-7.18429-5.68536-10.98375-2.94419-3.95978,2.85684-4.40562,8.09368-3.36503,12.48745,.63378,2.67602,1.65266,5.29118,2.6765,7.83978,.87047,2.16683,1.84546,4.3184,3.17656,6.24615,2.6185,3.79221,6.56207,6.27436,11.28073,6.03263,4.95423-.25379,9.33462-3.00957,12.42751-6.77246,3.77097-4.58787,6.06946-10.3746,7.90237-15.96445,2.07519-6.32874,3.32934-12.90027,3.82732-19.53899,.07223-.96288-1.42812-.95825-1.5,0-.74496,9.93139-3.20394,20.04629-7.77523,28.93617-2.07757,4.04029-4.87685,7.93778-8.96377,10.12785-3.86989,2.07377-8.72806,2.49879-12.47997-.0623-3.46799-2.36728-5.32423-6.47642-6.78884-10.26631-1.63486-4.23046-3.81938-9.23633-2.34844-13.80326,.63763-1.9797,2.00611-3.92282,3.99719-4.71236,2.07019-.82091,4.32818-.15697,5.97527,1.24272,3.89475,3.30976,3.27051,8.88073,1.38336,13.10231-3.88123,8.6824-12.59401,14.88436-22.01409,15.92982-.94888,.10531-.95959,1.6065,0,1.5H.71568Z"
+						fill="#01010C"
+					/>
+				</svg>
+			</div>
+		</>
+	);
+}
+
+export default Home;
