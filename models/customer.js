c0695a234dbf9d7dff0d3834e672313a52b8c398
durosly <slyboydon1@gmail.com>
2023-11-02 15:18:39 +0100

    📌update phonenumber validation

--- models/customer.js
+++ models/customer.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import paginate from "mongoose-paginate-v2";
 import { z } from "zod";
+import { phone } from "phone";
 
 const customerSchema = new mongoose.Schema({
 	name: String,
@@ -18,7 +19,15 @@ customerSchema.index({ name: "text", email: "text", phone: "text" });
 const CustomerValidationSchema = z.object({
 	name: z.string().min(3),
 	email: z.string().email(),
-	phone: z.string().min(2),
+	phone: z
+		.custom((number) => {
+			const res = phone(number, { country: "NG" });
+			return res.isValid;
+		})
+		.transform((number) => {
+			const res = phone(number, { country: "NG" });
+			return res.phoneNumber;
+		}),
 });
 
 export { CustomerValidationSchema };
