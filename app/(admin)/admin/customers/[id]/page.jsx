ec6a57bb9b34d3d7bb4278b850f617f8511dc56c
durosly <slyboydon1@gmail.com>
2023-11-03 17:45:32 +0100

    📌update customer's details page to use components

--- app/(admin)/admin/customers/[id]/page.jsx
+++ app/(admin)/admin/customers/[id]/page.jsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import { FiTrash } from "react-icons/fi";
+import CustomerActivities from "./__components/customer-activities";
+import CustomerApartments from "./__components/customer-apartments";
 import CustomerType from "./__components/customer-type";
 import UserInfo from "./__components/user-info";
 
@@ -10,88 +10,10 @@ function AdminCustomerDetailsPage({ params: { id } }) {
 
 			<div className="flex flex-wrap gap-5 mb-5">
 				<CustomerType id={id} />
-				<div className="flex-1 card bg-base-100">
-					<div className="card-body">
-						<h2 className="card-title">Apartments</h2>
-						<ul className="list-inside list-disc">
-							<li className="list-item gap-2">
-								<Link
-									className="link link-hover"
-									href={`/admin/apartments/${"id"}`}
-								>
-									lorem ipsum
-								</Link>
-								<button className="btn btn-outline btn-xs btn-error ml-2">
-									<FiTrash />
-								</button>
-							</li>
-						</ul>
-						<div className="card-actions justify-end">
-							<button className="btn btn-outline btn-primary">
-								Add
-							</button>
-						</div>
-					</div>
-				</div>
+				<CustomerApartments id={id} />
 			</div>
 
-			<div className="card bg-base-100 mb-5">
-				<div className="card-body">
-					<h2 className="card-title">Activities</h2>
-					<div className="overflow-x-auto">
-						<table className="table table-zebra">
-							{/* head */}
-							<thead>
-								<tr>
-									<th></th>
-
-									<th>Job</th>
-									<th>Date/time</th>
-									<th>Action</th>
-								</tr>
-							</thead>
-							<tbody>
-								{/* row 1 */}
-								<tr>
-									<th>1</th>
-
-									<td>Quality Control Specialist</td>
-									<td>12/12/2023 11:30pm</td>
-									<td>
-										<button className="btn btn-outline btn-xs btn-error ml-2">
-											<FiTrash />
-										</button>
-									</td>
-								</tr>
-								{/* row 2 */}
-								<tr>
-									<th>2</th>
-
-									<td>Desktop Support Technician</td>
-									<td>12/12/2023 11:30pm</td>
-									<td>
-										<button className="btn btn-outline btn-xs btn-error ml-2">
-											<FiTrash />
-										</button>
-									</td>
-								</tr>
-								{/* row 3 */}
-								<tr>
-									<th>3</th>
-
-									<td>Tax Accountant</td>
-									<td>12/12/2023 11:30pm</td>
-									<td>
-										<button className="btn btn-outline btn-xs btn-error ml-2">
-											<FiTrash />
-										</button>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
+			<CustomerActivities id={id} />
 
 			<button className="btn btn-error btn-block">Destroy</button>
 		</>
