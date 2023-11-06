d929a2c0312ed831a4cc2b7c2b2f287a566b242d
durosly <slyboydon1@gmail.com>
2023-11-06 20:14:41 +0100

    ✅admin CRUD apartments

--- app/(admin)/admin/apartments/[id]/page.jsx
+++ app/(admin)/admin/apartments/[id]/page.jsx
@@ -6,9 +6,10 @@ import ApartmentDescription from "./__components/apartment-desc";
 import ApartmentFeatures from "./__components/apartment-features";
 import ApartmentImages from "./__components/apartment-images";
 import ApartmentVideo from "./__components/apartment-video";
+import ApartmentOccupant from "./__components/apartment-occupant";
+import ApartmentDestroyBtn from "./__components/apartment-destroy";
 
 function ApartmentDetailsPage({ params: { id } }) {
-	let status = "hidden";
 	return (
 		<>
 			<div className="mb-5">
@@ -30,45 +31,9 @@ function ApartmentDetailsPage({ params: { id } }) {
 
 			<ApartmentVideo id={id} />
 
-			<div className="card bg-base-100 mb-5">
-				<div className="card-body">
-					<h2 className="card-title">Owners/Occupants</h2>
-					<form
-						action="/nice"
-						className="relative"
-					>
-						<div className="form-control relative">
-							<input
-								type="text"
-								className="input input-bordered pl-12"
-							/>
-							<FiSearch className="w-5 h-5 absolute top-1/2 -translate-y-1/2 left-4 pointer-events-none" />
-						</div>
+			<ApartmentOccupant id={id} />
 
-						{/* dropdown suggestion */}
-						{/* <ul className="absolute shadow-md rounded-md bottom-0 left-0 translate-y-full bg-base-100 p-3 w-full space-y-2">
-							<li className="hover:bg-base-200">Nice</li>
-							<li className="hover:bg-base-200">Nice</li>
-							<li className="hover:bg-base-200">Nice</li>
-						</ul> */}
-					</form>
-					<ul>
-						<li className="flex gap-2 items-center">
-							<FiCheckCircle className="stroke-success" />
-							<span>Bathroom is nice</span>
-
-							<button className="btn btn-sm btn-square btn-error">
-								<FiTrash2 />
-							</button>
-						</li>
-					</ul>
-					<div className="card-actions">
-						<button className="btn btn-primary">Save</button>
-					</div>
-				</div>
-			</div>
-
-			<button className="btn btn-error btn-block">Destroy</button>
+			<ApartmentDestroyBtn id={id} />
 		</>
 	);
 }
--- app/api/admin/apartments/[id]/route.js
+++ app/api/admin/apartments/[id]/route.js
@@ -1,3 +1,4 @@
 import getApartment from "./get-apartment";
+import deleteApartment from "./delete-apartment";
 
-export { getApartment as GET };
+export { getApartment as GET, deleteApartment as DELETE };
