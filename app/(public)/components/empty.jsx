05870755b2afea13b40182efcd04829518af43ef
durosly <slyboydon1@gmail.com>
2023-11-06 13:35:15 +0100

    Update empty.jsx

--- app/(public)/components/empty.jsx
+++ app/(public)/components/empty.jsx
@@ -1,10 +1,12 @@
 import { FiPackage } from "react-icons/fi";
 
-function Empty({ message }) {
+function Empty({ message, center = true }) {
 	return (
 		<div>
-			<FiPackage className="w-20 h-20 mx-auto" />
-			<p className=" font-bold text-center">{message}</p>
+			<FiPackage className={`w-20 h-20 ${center ? "mx-auto" : ""} `} />
+			<p className={`font-bold ${center ? "text-center" : ""} `}>
+				{message}
+			</p>
 		</div>
 	);
 }
