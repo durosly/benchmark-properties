322ce50795090ddebfc8fabe07f991231c790372
durosly <slyboydon1@gmail.com>
2023-11-07 09:03:09 +0100

    ✅display properties/apartments

--- app/(public)/rooms/__components/images.jsx
+++ app/(public)/rooms/__components/images.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 function ImageSliderContainer() {
 	return (
 		<>
-			<div className="relative w-full h-80">
+			<div className="relative w-full h-80 z-10">
 				<Image
 					src="https://images.pexels.com/photos/2111768/pexels-photo-2111768.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 					fill
